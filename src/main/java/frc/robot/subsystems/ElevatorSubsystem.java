aa0e754c3fd89e510b8b3d35d309fa49575d71f6
Super Leo <zhizhu0730@163.com>
2025-02-14 16:05:40 +0800

    create empty project for testing the elevator

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorSubsystem extends SubsystemBase{
+    private final TalonFX leader = new TalonFX(51, "canivore");
+    private final TalonFX follower = new TalonFX(52, "canivore");
+    TalonFXConfiguration leaderconfigs = new TalonFXConfiguration();
+    TalonFXConfiguration followerconfigs = new TalonFXConfiguration();
+    
+    public ElevatorSubsystem(){
+        leader.getConfigurator().apply(leaderconfigs);
+        follower.getConfigurator().apply(followerconfigs);
+    }
+
+    public void ShutDown(){
+        leader.setControl(new NeutralOut());
+        follower.setControl(new NeutralOut());
+    }
+
+    public void RunVolts(double volts){
+        leader.setControl(new VoltageOut(volts));
+        follower.setControl(new VoltageOut(volts));
+    }
+   
+
+}
