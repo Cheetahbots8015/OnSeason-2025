dd3775497be94c37736c42186c1c3ea46917e569
Super Leo <zhizhu0730@163.com>
2025-02-15 16:45:10 +0800

    add shutdown

--- src/main/java/frc/robot/commands/PivotForwardCommand.java
+++ src/main/java/frc/robot/commands/PivotForwardCommand.java
@@ -36,6 +36,7 @@ public class PivotForwardCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_subsystem.shutDown();
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/commands/PivotReverseCommand.java
+++ src/main/java/frc/robot/commands/PivotReverseCommand.java
@@ -36,7 +36,9 @@ public class PivotReverseCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_subsystem.shutDown();
   }
+  
 
   // Returns true when the command should end.
   @Override
