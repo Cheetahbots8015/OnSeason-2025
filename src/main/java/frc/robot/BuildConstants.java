d5d0fd76414cd501c9ccae0023d45ae390c23a9e
qiwueyrt <68145229+qiwueyrt@users.noreply.github.com>
2025-02-16 20:24:43 +0800

    Undo Mac's dummy error step 1: clear Generated path

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 21;
-  public static final String GIT_SHA = "883d30181031e2c7c46a5882ec6777da56a6244c";
-  public static final String GIT_DATE = "2025-02-16 03:55:05 EST";
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "c0bf510edf77d58f781ab2eadd37791d5409671b";
+  public static final String GIT_DATE = "2025-02-16 06:47:21 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-16 05:13:22 EST";
-  public static final long BUILD_UNIX_TIME = 1739700802978L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-16 07:22:56 EST";
+  public static final long BUILD_UNIX_TIME = 1739708576490L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
--- src/main/java/frc/robot/Generated/JoystickConstants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.generated;
-
-public class JoystickConstants {
-  public static final int driverControllerPort = 0;
-  public static final int testControllerPort = 1;
-  public static final int sysIDControllerPort = 2;
-}
