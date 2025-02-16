69acf029b5579b467fcd153169b8c99ea4d0ee31
qiwueyrt <68145229+qiwueyrt@users.noreply.github.com>
2025-02-16 20:25:24 +0800

    Undo Mac's dummy fault step 2: redo generated path

---
+++ src/main/java/frc/robot/generated/RollerConstants.java
@@ -0,0 +1,23 @@
+package frc.robot.generated;
+
+public class RollerConstants {
+  public static final int rollerID = 62;
+  public static final String canName = "canivore";
+  public static final int canRangeID = 1;
+  public static boolean inverted = false;
+  public static final double manualForwardVoltage = 8.0;
+  public static final double manualReverseVoltage = -2.0;
+  public static final double stationVoltage = 2.0;
+  public static final double L1Voltage = 6.0;
+  public static final double L2Voltage = 8.0;
+  public static final double L3Voltage = 8.0;
+  public static final double L4Voltage = 8.0;
+  public static final double LolipopVoltage = 8.0;
+  public static final double coralVelocity = 0.0;
+  public static final double kP = 0.0;
+  public static final double kI = 0.0;
+  public static final double kD = 0.0;
+  public static final double kA = 0.0;
+  public static final double kS = 0.0;
+  public static final double kV = 0.0;
+}
