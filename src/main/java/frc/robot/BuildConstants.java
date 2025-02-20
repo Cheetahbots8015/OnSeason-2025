2d3accf5dc044ddfd9bff42c45a4a7c402aed85e
JasonG25 <guozichen_jason@qq.com>
2025-02-20 15:53:14 +0800

    nothing changed, just builded

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 57;
-  public static final String GIT_SHA = "65fe3a53de902d83ba7c5dfad80f44b6562a7491";
-  public static final String GIT_DATE = "2025-02-19 09:57:44 EST";
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "815d28c687026f4c6efa758626c83ee36b4947a9";
+  public static final String GIT_DATE = "2025-02-20 02:29:44 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-20 01:40:01 EST";
-  public static final long BUILD_UNIX_TIME = 1740033601478L;
+  public static final String BUILD_DATE = "2025-02-20 02:45:37 EST";
+  public static final long BUILD_UNIX_TIME = 1740037537773L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
