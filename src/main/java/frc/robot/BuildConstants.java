4788bb9d6e7d9db45b9800ffbecf1ccb757277fd
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-02-21 17:50:52 +0800

    Develop joystick rumble

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 70;
-  public static final String GIT_SHA = "95f983d1b578fb8ade397916678090682482f97b";
-  public static final String GIT_DATE = "2025-02-20 05:13:28 EST";
+  public static final int GIT_REVISION = 71;
+  public static final String GIT_SHA = "ca1ccb4f53759d499ffe6475d06897ea7d6915fc";
+  public static final String GIT_DATE = "2025-02-20 05:16:40 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-20 05:15:37 EST";
-  public static final long BUILD_UNIX_TIME = 1740046537689L;
+  public static final String BUILD_DATE = "2025-02-21 04:43:09 EST";
+  public static final long BUILD_UNIX_TIME = 1740130989395L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -197,6 +197,7 @@ public class RobotContainer {
     drive.setDefaultCommand(
         DriveCommands.joystickDrive(
             drive,
+            driverController,
             () -> -driverController.getLeftY(),
             () -> -driverController.getLeftX(),
             () -> -driverController.getRightX()));
