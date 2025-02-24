af8ce257cf1ef4daecca7db2bab25714d3a385ac
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-02-24 15:58:19 -0800

    Fix something

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 85;
-  public static final String GIT_SHA = "e623de480811e269aea31fe69199291a7393148f";
-  public static final String GIT_DATE = "2025-02-23 20:57:02 EST";
+  public static final int GIT_REVISION = 93;
+  public static final String GIT_SHA = "1aebe0a69989b85b64367af648290d12cef3fe1f";
+  public static final String GIT_DATE = "2025-02-24 18:40:00 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-24 13:20:49 EST";
-  public static final long BUILD_UNIX_TIME = 1740421249837L;
+  public static final String BUILD_DATE = "2025-02-24 18:41:52 EST";
+  public static final long BUILD_UNIX_TIME = 1740440512419L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
---
+++ src/main/java/frc/robot/commands/alignreef.java
@@ -0,0 +1,77 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.LimelightHelpers;
+import frc.robot.subsystems.drive.Drive;
+
+public class alignreef extends Command {
+  private final Drive m_drive;
+  private final String m_direction;
+  private final CommandXboxController m_controller;
+  private PIDController pidy;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public alignreef(Drive drive, String direction, CommandXboxController controller) {
+    m_drive = drive;
+    m_direction = direction;
+    m_controller = controller;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    LimelightHelpers.setPipelineIndex("limelight-reef", 1);
+    LimelightHelpers.SetFidcuial3DOffset("limelight-reef", 0, 0.5, 0);
+    LimelightHelpers.setCameraPose_RobotSpace("limelight-reef", 0, -0.25, 0.82, 0, -30, 0);
+    LimelightHelpers.setLEDMode_ForceOff("limelight-reef");
+    pidy = new PIDController(0.07, 0, 0);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    boolean hasTarget = LimelightHelpers.getTV("limelight-reef");
+    Pose3d pose = LimelightHelpers.getTargetPose3d_RobotSpace("limelight-reef");
+    if (hasTarget) {
+      if (Math.abs(LimelightHelpers.getTX("limelight-reef")) < 1) {
+        m_controller.setRumble(RumbleType.kBothRumble, 0.5);
+      } else {
+        m_controller.setRumble(RumbleType.kBothRumble, 0);
+      }
+      SmartDashboard.putBoolean("limelight-reef", false);
+      ChassisSpeeds drivSpeeds =
+          new ChassisSpeeds(0, pidy.calculate(LimelightHelpers.getTX("limelight-reef")), 0);
+      m_drive.runVelocity(drivSpeeds);
+    } else {
+      ChassisSpeeds drivSpeeds = new ChassisSpeeds(0, 0, 0);
+      m_drive.runVelocity(drivSpeeds);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drive.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return Math.abs(LimelightHelpers.getTX("limelight-reef")) < 0.8;
+  }
+}
