fcc3d3c185385c04d7d8101ada978bd91f371277
Super Leo <zhizhu0730@163.com>
2025-02-24 17:20:44 -0800

    change end methods of commands and constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 85;
-  public static final String GIT_SHA = "e623de480811e269aea31fe69199291a7393148f";
-  public static final String GIT_DATE = "2025-02-23 20:57:02 EST";
+  public static final int GIT_REVISION = 90;
+  public static final String GIT_SHA = "efd424463345521d6a9ac015dc1e4e026d2aeb74";
+  public static final String GIT_DATE = "2025-02-24 13:21:03 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-24 13:20:49 EST";
-  public static final long BUILD_UNIX_TIME = 1740421249837L;
+  public static final String BUILD_DATE = "2025-02-24 19:09:38 EST";
+  public static final long BUILD_UNIX_TIME = 1740442178949L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
