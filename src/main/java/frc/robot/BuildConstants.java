12658c15071d45a0631732555d8a9c094d1c2e63
JasonG25 <guozichen_jason@qq.com>
2025-02-26 07:50:29 +0800

    added some dashboard

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 113;
-  public static final String GIT_SHA = "4c53c990ac46d8449be6e1af4dd5f92e230388ce";
-  public static final String GIT_DATE = "2025-02-25 01:12:28 EST";
+  public static final int GIT_REVISION = 114;
+  public static final String GIT_SHA = "1ef5f6e2bd662ca8ae2a9da134e8d4115ba10d34";
+  public static final String GIT_DATE = "2025-02-25 12:59:40 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-25 12:58:26 EST";
-  public static final long BUILD_UNIX_TIME = 1740506306702L;
+  public static final String BUILD_DATE = "2025-02-25 18:43:06 EST";
+  public static final long BUILD_UNIX_TIME = 1740526986626L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
