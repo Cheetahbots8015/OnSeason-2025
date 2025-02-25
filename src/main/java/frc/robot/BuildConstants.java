39944c1a779304ea0a5499ad48143540dc1f7d64
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-02-24 17:23:59 -0800

    Update something

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 93;
-  public static final String GIT_SHA = "1aebe0a69989b85b64367af648290d12cef3fe1f";
-  public static final String GIT_DATE = "2025-02-24 18:40:00 EST";
+  public static final int GIT_REVISION = 95;
+  public static final String GIT_SHA = "a2b91209032b5443aa2100972c906f68eb8f0ade";
+  public static final String GIT_DATE = "2025-02-24 19:31:27 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-24 18:41:52 EST";
-  public static final long BUILD_UNIX_TIME = 1740440512419L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-24 20:14:59 EST";
+  public static final long BUILD_UNIX_TIME = 1740446099165L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
