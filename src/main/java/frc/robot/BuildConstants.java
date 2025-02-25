9940aad986923fa61958c6fd5c6a91e24ea67dfb
EastGu <eastgugugu@gmail.com>
2025-02-25 09:30:36 +0800

    AlignReef.java looks obviously better than alignreef.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 94;
-  public static final String GIT_SHA = "af8ce257cf1ef4daecca7db2bab25714d3a385ac";
-  public static final String GIT_DATE = "2025-02-24 18:58:19 EST";
+  public static final int GIT_REVISION = 97;
+  public static final String GIT_SHA = "c86973b3caf3fc5cd768ebff2f262158773a89d1";
+  public static final String GIT_DATE = "2025-02-24 20:12:17 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-24 19:53:25 EST";
-  public static final long BUILD_UNIX_TIME = 1740444805887L;
+  public static final String BUILD_DATE = "2025-02-24 20:17:49 EST";
+  public static final long BUILD_UNIX_TIME = 1740446269428L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
