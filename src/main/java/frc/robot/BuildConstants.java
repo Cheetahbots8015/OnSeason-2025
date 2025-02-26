1f2cec0a5973586ed834802499bc94cac21843c6
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-02-25 18:03:48 -0800

    Improve odometry logic
    Prefer reef limelight instead of station limelight

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 115;
-  public static final String GIT_SHA = "12658c15071d45a0631732555d8a9c094d1c2e63";
-  public static final String GIT_DATE = "2025-02-25 18:50:29 EST";
+  public static final int GIT_REVISION = 122;
+  public static final String GIT_SHA = "aee3db7e3cc4c17fa601db7eedb99a987a0885a4";
+  public static final String GIT_DATE = "2025-02-25 20:56:18 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-25 18:58:21 EST";
-  public static final long BUILD_UNIX_TIME = 1740527901327L;
+  public static final String BUILD_DATE = "2025-02-25 21:01:34 EST";
+  public static final long BUILD_UNIX_TIME = 1740535294206L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
