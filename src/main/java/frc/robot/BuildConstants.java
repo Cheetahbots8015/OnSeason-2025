bd336a15cd2d5d9e6859103ba1fcb877d3c92778
JasonG25 <guozichen_jason@qq.com>
2025-03-01 00:01:05 +0800

    added some auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 154;
-  public static final String GIT_SHA = "25f8bf0503a6835d4497b48620b6e9f3d6238bb5";
-  public static final String GIT_DATE = "2025-02-28 02:45:53 EST";
+  public static final int GIT_REVISION = 155;
+  public static final String GIT_SHA = "5a317fe3452ef1ccb7ce27bd9d5ee5b876f2a479";
+  public static final String GIT_DATE = "2025-02-28 10:46:35 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-28 10:45:23 EST";
-  public static final long BUILD_UNIX_TIME = 1740757523436L;
+  public static final String BUILD_DATE = "2025-02-28 10:47:52 EST";
+  public static final long BUILD_UNIX_TIME = 1740757672235L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
