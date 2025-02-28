1072ec438f8eddae4cb49bebfce534387a3484d9
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-02-27 21:42:20 -0800

    Add SmartDashboard

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 143;
-  public static final String GIT_SHA = "2cf5f0d1281f38ccd9df7c34c141e185ded0e2ff";
-  public static final String GIT_DATE = "2025-02-27 17:55:20 EST";
+  public static final int GIT_REVISION = 146;
+  public static final String GIT_SHA = "5dc263a5b5d779045596392a5e5f975141f25b15";
+  public static final String GIT_DATE = "2025-02-27 19:56:54 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-27 19:46:48 EST";
-  public static final long BUILD_UNIX_TIME = 1740703608483L;
+  public static final String BUILD_DATE = "2025-02-28 00:39:44 EST";
+  public static final long BUILD_UNIX_TIME = 1740721184766L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/commands/driverCommand/DriveCommands.java
+++ src/main/java/frc/robot/commands/driverCommand/DriveCommands.java
@@ -17,6 +17,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -26,9 +27,11 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.drive.Drive;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -74,8 +77,19 @@ public class DriveCommands {
       DoubleSupplier ySupplier,
       DoubleSupplier omegaSupplier,
       boolean slowMode) {
+    LimelightHelpers.setPipelineIndex("limelight-reef", 0);
+    LimelightHelpers.setCameraPose_RobotSpace("limelight-reef", 0, -0.25, 0.8, 0, -30, 0);
     return Commands.run(
         () -> {
+          if (!LimelightHelpers.getTV("limelight-reef")) {
+            SmartDashboard.putBoolean("Aiming left", false);
+            SmartDashboard.putBoolean("Aiming right", false);
+          }
+          Pose3d pose = LimelightHelpers.getTargetPose3d_RobotSpace("limelight-reef");
+          SmartDashboard.putBoolean(
+              "Aiming left", Math.abs(pose.getTranslation().getY() + 0.165) < 0.05);
+          SmartDashboard.putBoolean(
+              "Aiming right", Math.abs(pose.getTranslation().getY() - 0.165) < 0.05);
           // Get linear velocity
           Translation2d linearVelocity =
               getLinearVelocityFromJoysticks(xSupplier.getAsDouble(), ySupplier.getAsDouble());
