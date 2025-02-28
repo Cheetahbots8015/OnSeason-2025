aecb9db8b2f1759af10b95e49d51a97b08d4caa3
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-02-27 22:43:01 -0800

    Fix humanbenchmark

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OnSeason-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 146;
-  public static final String GIT_SHA = "5dc263a5b5d779045596392a5e5f975141f25b15";
-  public static final String GIT_DATE = "2025-02-27 19:56:54 EST";
+  public static final int GIT_REVISION = 147;
+  public static final String GIT_SHA = "1072ec438f8eddae4cb49bebfce534387a3484d9";
+  public static final String GIT_DATE = "2025-02-28 00:42:20 EST";
   public static final String GIT_BRANCH = "old-style-workable";
-  public static final String BUILD_DATE = "2025-02-28 00:39:44 EST";
-  public static final long BUILD_UNIX_TIME = 1740721184766L;
+  public static final String BUILD_DATE = "2025-02-28 01:39:12 EST";
+  public static final long BUILD_UNIX_TIME = 1740724752215L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
